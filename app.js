9e2d93ef838f1fee992a7a3417dd1b8f162d3c59
Magesh <mageshmurugan64@gmail.com>
2022-08-04 18:50:56 +0530

    add start script

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const MongoDBStore = require("connect-mongo");
 const dbUrl = process.env.DB_URL //|| 'mongodb://localhost:27017/yelp-camp';
 
 // 'mongodb://localhost:27017/yelp-camp'
-mongoose.connect(dbUrl, {
+mongoose.connect(dbUrl, client, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
