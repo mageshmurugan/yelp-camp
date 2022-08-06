78917747a46fd4a4c0b8170643812de1921f16bd
Magesh <mageshmurugan64@gmail.com>
2022-08-06 22:52:18 +0530

    add a script

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 // const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const MongoDBStore = require("connect-mongo")(session);
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
-const MongoDBStore = require("connect-mongo/es5")(session);
 
 const dbUrl = process.env.DB_URL //|| 'mongodb://localhost:27017/yelp-camp';
 
