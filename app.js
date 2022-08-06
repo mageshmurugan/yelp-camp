2f3385aab0ba8f7fae68dfe300c07b76cc9c4d44
Magesh <mageshmurugan64@gmail.com>
2022-08-06 20:49:54 +0530

    add a script

--- app.js
+++ app.js
@@ -103,7 +103,7 @@ app.use(session({
         maxAge: 1000 * 60 * 60 * 24 * 7
     },
     // mongoUrl: dbUrl,
-    client: db.getClient(),
+    // client: db.getClient(),
     store: store
     // store: MongoStore.create({
     //     mongoUrl: dbUrl,
