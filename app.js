6385b2b2a42e9098556a42f77524253bdfe4d0ab
Magesh <mageshmurugan64@gmail.com>
2022-08-06 21:28:29 +0530

    add a script

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const reviewRoutes = require('./routes/reviews');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }))
-const MongoDBStore = require("connect-mongodb-session")(session);
+const MongoStore = require("connect-mongo");
 
 const dbUrl = process.env.DB_URL //|| 'mongodb://localhost:27017/yelp-camp';
 
@@ -62,10 +62,10 @@ app.use(mongoSanitize({
 
 const secret = process.env.SECRET || 'thisshouldbeabettersecret';
 
-const store = new MongoDBStore({
-    uri: dbUrl,
-    collection: 'mySessions',
-    touchAfter: 24 * 60 * 60,
+const store = MongoStore.create({
+    mongoUrl: dbUrl,
+    collectionName: 'sessions',
+    // touchAfter: 24 * 60 * 60,
     databaseName: "cluster0"
     // secret: 'thisshouldbeabettersecret'
 },
