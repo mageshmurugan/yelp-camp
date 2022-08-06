5623a25f29e7191436a736425f033509d03873c8
Magesh <mageshmurugan64@gmail.com>
2022-08-06 19:20:48 +0530

    add a script

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ const dbUrl = process.env.DB_URL //|| 'mongodb://localhost:27017/yelp-camp';
 // 'mongodb://localhost:27017/yelp-camp'
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
-    useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true,
-    useFindAndModify: false
+    // useFindAndModify: false
 });
 
 const db = mongoose.connection;
